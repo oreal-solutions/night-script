3645c2fa0cb0fb2b159c0775b4856c2a1225fb67
Comfort-Twala <kontreitroos@gmail.com>
2021-05-30 00:03:36 +0200

    App Method Modified!

--- src/main/java/com/nightscript/App.java
+++ src/main/java/com/nightscript/App.java
@@ -1,13 +1,42 @@
 package com.nightscript;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Main Application to run langauge!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static File script;
+    private static Scanner reader;
+    private static List<Line> lines;
+    public static void main( String[] args ) throws FileNotFoundException
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to NightScript!" );
+        // OPENING FILE 
+        script = new File(args[0]);
+        reader = new Scanner(script);
+        String content = "";
+        while (reader.hasNextLine()){
+            content.concat(reader.nextLine());
+        }
+        lines = new LineSplitter().getLines(content); 
+        
+        // Main loop to run program line by line.
+        for (Line line : lines){
+            handleLine(line);
+        }
+    }
+
+    /**
+     * Master method to evaluate each line and decides what happens to it
+     * @param line
+     */
+    private static void handleLine(Line line){
+
     }
 }
