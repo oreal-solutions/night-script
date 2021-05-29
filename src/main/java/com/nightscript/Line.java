4e23353a384078751c05c6517396793f8bb1b05f
Comfort-Twala <kontreitroos@gmail.com>
2021-05-29 22:18:53 +0200

    Line class added

---
+++ src/main/java/com/nightscript/Line.java
@@ -0,0 +1,11 @@
+package com.nightscript;
+
+public class Line {
+    final public int lineNumber;
+    final public String lineContent;
+
+    public Line(int lineNumber, String lineContent){
+       this.lineNumber = lineNumber;
+       this.lineContent = lineContent;
+    }
+}
